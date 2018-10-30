dd2af60a9c9a6e997049cc869d9a5ca32c2bfad1
Jennifer <jenniferyhwu@gmail.com>
2018-10-29 22:24:58 -0400

    first commit

---
+++ app/src/main/java/com/example/jennifer/app_practice/BatteryScreen.java
@@ -0,0 +1,13 @@
+package com.example.jennifer.app_practice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BatteryScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battery_screen);
+    }
+}
